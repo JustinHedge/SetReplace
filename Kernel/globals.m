6c6786b4ea4ad8928eca417ff58e0d3745770ac8
Taliesin Beynon <github@taliesin.ai>
2020-11-09 13:02:40 +0200

    General improvements to Kernel/init.m. Use BeginPackage and Begin to do proper context isolation to make loading GU safer, which we now do, fixing issue #508. Also include some useful sugar that isn't funky syntax which the previous override of $DesugaringRules unintentionally removed. Define the package global $SetReplaceBaseDirectory, which other PRs will end up using in various places.

---
+++ Kernel/globals.m
@@ -0,0 +1,5 @@
+Package["SetReplace`"]
+
+(* Note: this is actually set in Kernel/init.m, but it needs to be exported from a new-style package so that
+the new-style package loader understands that this is a global symbol *)
+PackageExport["$SetReplaceBaseDirectory"]
